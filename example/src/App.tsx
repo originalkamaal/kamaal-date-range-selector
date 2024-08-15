8d3fdd79321bd9bc1de12f29c14709de94398137
CodingKamaal <158290671+CodingKamaal@users.noreply.github.com>
2024-08-15 20:24:28 +0530

    init

---
+++ example/src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import Calender from 'kamaal-date-range-selector'
+import 'kamaal-date-range-selector/dist/index.css'
+
+const App = () => {
+  return <Calender text="Create React Library Example 😄" />
+}
+
+export default App
